cf0885105b007d265df7a957d50e597fc44f8662
Emgenari <emgenari@gmail.com>
2021-02-15 16:09:25 +0100

    15-02-a

--- src/com/front/view/MainApp.java
+++ src/com/front/view/MainApp.java
@@ -1,6 +1,5 @@
 package com.front.view;
 
-
 //import com.front.domain.Window;
 import com.crud.controller.BusinessController;
 import com.crud.controller.BusinessController.Color;
@@ -14,23 +13,35 @@ public abstract class MainApp {
 		//w.setVisible(true);
 
 		//CONSOLE TESTS AVAIBLE
-
-		BusinessController c = new BusinessController();		//controller instance
-		c.createFlowerShop("Navarro");							//store creation
-		c.getAllFlowerShops().forEach(System.out::println);		//show stock: zeroed for now 
-		c.createDecoration("Decor1", Material.Plastic, 20.0, "1");		//products creations: 
-		c.createFlower("Rose", Color.Blue, 5.0, "1");
-		c.createTree("Roble", 1.5, 33.50, "naVaRro");
-		c.createFlower("Jacinto", Color.Red, 2.0, "NAVARRO");
-		c.createDecoration("Decor2", Material.Wood, 10.0, "navarro");
-		c.createTree("Primavera", 2.3, 50.75, "1");
-		c.createFlower("jasmin", Color.Yellow, 3.0, "Navarro");
-		c.createTree("Erro", 1.5, 33.50, "one");						//product creation: with error!
-		c.getAllFlowerShops().forEach(System.out::println);		//show stock: fully now 
-		c.showStock("one");										//stock detailing
-		c.showStock("1");										//stock detailing: with error!
-
-
+		//controller instance
+		BusinessController c = new BusinessController();
+		//create stores
+		c.createFlowerShop("Petalon");
+		c.createFlowerShop("McQueens");
+		//read stores. empty
+		c.getAllFlowerShops().forEach(System.out::println);		
+		//create flower
+		c.createFlower("Rose", Color.Red, 7.5, "1");
+		c.createFlower("Rose", Color.Yellow, 7.5, "Petalon");
+		c.createFlower("Daisy", Color.Blue, 3.5, "petalon");
+		c.createFlower("Sunflower", Color.Yellow, 6.5, "1");
+		c.createFlower("Sunflower", Color.Yellow, 6.5, "mcqueens");
+		c.createFlower("Alstroemerias", Color.Yellow, 9, "2");
+		//create tree		
+		c.createTree("Oak", 1.5, 75.50, "PETALON");
+		c.createTree("Orange Tree", 1.3, 55.0, "1");
+		c.createTree("Lemon Tree", 1.1, 45.0, "2");
+		//create decoration
+		c.createDecoration("Box", Material.Wood, 12.5, "1");		
+		c.createDecoration("Vase", Material.Plastic, 10.0, "petALON");
+		//create decoration. error. store not found	
+		c.createDecoration("Gift Lace", Material.Plastic, 7.50, "one");						
+		//read stores. completed
+		c.getAllFlowerShops().forEach(System.out::println);		
+		//read stock. error. store not found
+		c.showStock("two");		
+		//read stock
+		c.showStock("1");
+		c.showStock("2");
 	}
-
 }
